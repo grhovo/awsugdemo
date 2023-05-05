80bd850312ea29d9813df18d9573c9b4279ce592
hovo <hovhannes.grigoryan@disqo.com>
2023-05-05 21:10:29 +0400

    get access token

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import routeItems from './data/routesItems';
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-// import { Auth } from 'aws-amplify';
-
-Amplify.configure({
-  Auth: {
-    region: awsExports.REGION,
-    userPoolId: awsExports.USER_POOL_ID,
-    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
-  }
-});
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import routeItems from './data/routesItems';
+import { Auth } from 'aws-amplify';
 
 function App() {
+
+  Auth.currentSession().then(res=>{
+    let accessToken = res.getAccessToken()
+    let jwt = accessToken.getJwtToken()
+        
+    //You can print them to see the full objects
+    console.log(`myAccessToken: ${JSON.stringify(accessToken)}`)
+    console.log(`myJwt: ${jwt}`)
+  })
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div>
-          <p>Welcome {user.username}</p>
-          <button onClick={signOut}>Sign out</button>
-        </div>
-      )}
-      {/* <Router>
+    <>
+      <Router>
         <Routes>
           {
             routeItems.map((route) => {
@@ -48,8 +40,8 @@ function App() {
             })
           }
         </Routes>
-      </Router> */}
-    </Authenticator>
+      </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,13 +7,31 @@ import 'antd/dist/antd.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+Amplify.configure({
+  Auth: {
+    region: awsExports.REGION,
+    userPoolId: awsExports.USER_POOL_ID,
+    userPoolWebClientId: awsExports.USER_POOL_APP_CLIENT_ID
+  }
+});
+ 
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ Provider>
+  <Authenticator>
+    {() => (
+      <Provider store={store}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ Provider>
+    )}
+  </Authenticator>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
