d5551daf8bd77b114ccac57a9920067552359c2d
hovo <hovhannes.grigoryan@disqo.com>
2023-05-05 20:52:18 +0400

    Fix client id

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,5 +1,5 @@
 export default {
   'REGION': 'us-west-1',
   'USER_POOL_ID': 'us-west-1_IoCCPAplB',
-  'USER_POOL_APP_CLIENT_ID': '3iqi0qeh3fqmoaali78kfppk3e'
+  'USER_POOL_APP_CLIENT_ID': '6vnlnfm2c20nb7t6rst30nqog0'
 };
