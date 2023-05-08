d499fdacc61b86bc5a0e3005ad63254ed616e8d4
Serine-t <serinetovmasyan98@gmail.com>
2023-05-08 22:25:17 +0400

    add config for getting access token

--- src/store/actions/textActions.js
+++ src/store/actions/textActions.js
@@ -1,12 +1,13 @@
 import { API_LINK } from '../../data/API';
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { config } from '../../constants';
+import { gettingAccessToken } from '../../helpers';
 
 export const getTextInfo = createAsyncThunk(
   'market/getMarketInfo',
   async (id, thunkAPI) => {
     try {
+      const config = await gettingAccessToken();
       const resp = await axios(`${API_LINK}/text?id=${id}`, config);
       
       return resp.data;
--- src/store/actions/videoActions.js
+++ src/store/actions/videoActions.js
@@ -1,12 +1,13 @@
 import { API_LINK } from '../../data/API';
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { config } from '../../constants';
+import { gettingAccessToken } from '../../helpers';
 
 export const getVideoInfo = createAsyncThunk(
   'video/getVideoInfo',
   async (_, thunkAPI) => {
     try {
+      const config = await gettingAccessToken();
       const resp = await axios(`${API_LINK}/video`, config);
       
       return resp.data;
